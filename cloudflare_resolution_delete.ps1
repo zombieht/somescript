09218a90f39b6de9a6f0b860cb4ad18728a39f9d
zombieht <zombie19931113@gmail.com>
2023-06-29 00:57:10 +0800

    cloudflare批量删除dns解析

---
+++ cloudflare_resolution_delete.ps1
@@ -0,0 +1,22 @@
+$API_TOKEN = "<token>"
+$ZONE_ID   = "<ZONE_ID>"
+
+$baseUrl = "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records"
+
+$headers = @{
+  'Authorization' = "Bearer $API_TOKEN"
+  'Content-Type'  = "application/json"
+}
+
+$listUrl = $baseUrl + '?per_page=500'
+Write-Host $listUrl
+$records = Invoke-RestMethod -Uri $listUrl -Method 'GET' -Headers $headers
+$records = $records | Select-Object -ExpandProperty result
+
+foreach ($record in $records) {
+  Write-Host "Deleting $($record.name) that points to $($record.content)"
+
+  $deleteUrl = $baseUrl + '/' + $record.id
+  Invoke-RestMethod -Uri $deleteUrl -Method 'DELETE' -Headers $headers
+  Write-Host $deleteUrl
+}
